163fbfa0f944773face86687515c67df56431bbb
Matias Repetto Zecchi <60527450+MatiasRepetto@users.noreply.github.com>
2021-09-13 00:52:18 -0300

    Add files via upload

--- Algoritmo1_problema_p2.m
+++ Algoritmo1_problema_p2.m
@@ -1,21 +1,33 @@
-% Algoritmo 1: Algoritmo para (p2), Grupo L,R,P,R %
+% Algoritmo 1: Algoritmo para (p2), Grupo Ledesma,Repetto,Porcal,Rocha 
 
-clear %limpia posibles variables que usaremos%
-cantidadlambda = 3 %Numero de entradas del vector lambda%
-epsilon = 10^(-10) %epsilon maquina%
-Dx = [3,2,-1]  %Vector lambda%
-Y = rand(cantidadlambda) %Matriz Random Y positiva%
-Y = Y*Y' %Hace simetrica Y%
+clearvars %limpiamos variables
+Iteraciones = 0 
+cantidadlambda = 3 %Numero de entradas del vector lambda
+epsilon = 10^(-10) %epsilon maquina
+Dx = [32,12,1]  %Vector lambda
+Y = rand(cantidadlambda) %Matriz Random Y positiva
+Y = Y+Y' %Hace simetrica Y
+
 do
-  
+  Iteraciones = Iteraciones+1;
   [autovec,autoval] = eig(Y)
   Dy = autoval
-  Vy = orth(autovec) 
+  Vy = rot90(rot90(autovec)) 
   Vyt = Vy'
   Y = Vy*Dy*Vyt
   X = Vy*diag(Dx)*Vyt
   X = ((1/2)*(X + X'))
   Y = X
-  Y(Y<0)=0 
-  
-until norm((X-Y),"fro")<epsilon 
+  Y(Y<0)=0  
+until norm((X-Y),"fro")<epsilon %| Iteraciones>256
+
+
+printf("========================================================\n")
+
+Iteraciones
+X
+Y
+NormaDiferencia = norm((X-Y),"fro")
+AutovaloresRespuesta = eig(Y)
+
+printf("========================================================\n")
