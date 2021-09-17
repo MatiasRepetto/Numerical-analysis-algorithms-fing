ff638f33c2081a562a21e47080d24214da5fbc0c
Matias Repetto Zecchi <60527450+MatiasRepetto@users.noreply.github.com>
2021-09-17 20:36:58 -0300

    Add files via upload

--- Algoritmo1_problema_p2.m
+++ Algoritmo1_problema_p2.m
@@ -1,9 +1,12 @@
 % Algoritmo 1: Algoritmo para (p2), Grupo Ledesma,Repetto,Porcal,Rocha 
 
-clearvars %limpiamos variables
+%clearvars %limpiamos variables
+valido = 0
+encontro = 0
+noencontro = 0
 Iteraciones = 0 
 epsilon = 10^(-10) %epsilon maquina
-Dx = [3,1,-1,-1,-1,-1]  %Vector lambda
+Dx = [(rand-(1/3))*100,(rand-(1/3))*100,(rand-(1/3))*100,(rand-(1/3))*100]  %Vector lambda
 Dx = sort(Dx,'descend')
 Y = rand(length(Dx)) %Matriz Random Y positiva
 Y = Y+Y' %Hace simetrica Y
@@ -15,18 +18,17 @@ do
   Dy = autoval
   Vy = rot90(rot90(autovec))
   Vyt = Vy'
-  Y = Vy*Dy*Vyt
   X = Vy*diag(Dx)*Vyt
-  X = ((1/2)*(X + X'))
+  X = ((0.5)*(X + X'))
   Y = X
   Y(Y<0)=0
   
-until norm((X-Y),"fro")<epsilon | Iteraciones > 1000
+until (norm((X-Y),"fro") < epsilon) | (Iteraciones > 1000)
 
 if (Iteraciones <= 1000)
-  encontro = true;
+  encontro = encontro + 1;
 else
-  encontro = false;
+  noencontro = noencontro + 1;
 endif
  
 printf("========================================================\n")
