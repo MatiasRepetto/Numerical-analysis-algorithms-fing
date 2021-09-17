b6ac6c16a60b0583d015ee382a5168758d1a504a
Matias Repetto Zecchi <60527450+MatiasRepetto@users.noreply.github.com>
2021-09-17 00:41:25 -0300

    Add files via upload

--- Algoritmo1_problema_p2.m
+++ Algoritmo1_problema_p2.m
@@ -2,32 +2,40 @@
 
 clearvars %limpiamos variables
 Iteraciones = 0 
-cantidadlambda = 3 %Numero de entradas del vector lambda
 epsilon = 10^(-10) %epsilon maquina
-Dx = [32,12,1]  %Vector lambda
-Y = rand(cantidadlambda) %Matriz Random Y positiva
+Dx = [3,1,-1,-1,-1,-1]  %Vector lambda
+Dx = sort(Dx,'descend')
+Y = rand(length(Dx)) %Matriz Random Y positiva
 Y = Y+Y' %Hace simetrica Y
 
 do
+  
   Iteraciones = Iteraciones+1;
   [autovec,autoval] = eig(Y)
   Dy = autoval
-  Vy = rot90(rot90(autovec)) 
+  Vy = rot90(rot90(autovec))
   Vyt = Vy'
   Y = Vy*Dy*Vyt
   X = Vy*diag(Dx)*Vyt
   X = ((1/2)*(X + X'))
   Y = X
-  Y(Y<0)=0  
-until norm((X-Y),"fro")<epsilon %| Iteraciones>256
+  Y(Y<0)=0
+  
+until norm((X-Y),"fro")<epsilon | Iteraciones > 1000
 
-
+if (Iteraciones <= 1000)
+  encontro = true;
+else
+  encontro = false;
+endif
+ 
 printf("========================================================\n")
 
+Vectorlambda = Dx
 Iteraciones
 X
 Y
-NormaDiferencia = norm((X-Y),"fro")
+encontro
 AutovaloresRespuesta = eig(Y)
 
 printf("========================================================\n")
